a64c982ff299252bf312823773ee75e51edd351a
Bindumuniyappa <muniyappabindu7@gmail.com>
2022-12-14 23:32:47 +0530

    js_handson 11th

---
+++ handson_11th.js
@@ -0,0 +1,56 @@
+//  function abc(){
+//     console.log("Zero parameter")
+// }
+// abc();
+
+// function sum(a,b){
+//     sum=a+b;
+//     console.log(sum)
+// }
+// sum(3,4);
+
+// let sum=(x,y)=>{
+//     console.log(x+y);
+// }
+// sum(5,2);
+ 
+// var x = 21;
+// var girl = function () {
+//     console.log(x);
+//     var x = 20;
+// };
+// girl ();
+
+ 
+// var x = 21;
+// girl ();
+// console.log(x)
+// function girl() {
+//     console.log(x);
+//     var x = 20;
+// };
+
+// var x = 21;
+// a();
+// b();
+
+//   function a() {
+    
+//    x = 20;
+//   console.log(x);
+// };
+//  function b() {
+    
+//     x = 40;
+//    console.log(x);
+// };
+// function fact(x){
+//     if(x==0 || x==1 ){
+//         return 1;
+//     }
+//     else{
+//         return x*fact(x-1);
+//     }
+// }
+//  let n=fact(5)
+//  console.log(n);
